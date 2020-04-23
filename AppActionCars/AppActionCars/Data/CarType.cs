d6b4174b79333d08c964cb17ab10b6b2839c3e69
DiegoGarzon23 <garzoncdiego@hotmail.com>
2020-04-23 00:05:54 -0500

    IEntity

---
+++ AppActionCars/AppActionCars/Data/CarType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace AppActionCars.Data
+{
+    public class CarType
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ AppActionCars/AppActionCars/Data/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace AppActionCars.Data
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+    }
+}
